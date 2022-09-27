fcca138dbe7ceb4344e0417add6e97b4ea9b8bcc
Mario Cartagena <mario.cartagena@armonicolat.com>
2022-09-27 14:01:28 -0600

    Commit Inicial con Log In, dashboard vacio y crud usuarios

---
+++ vistas/modulos/inicio/cajas-superiores.php
@@ -0,0 +1,132 @@
+<?php
+
+$item = null;
+$valor = null;
+$orden = "id";
+/*
+$ventas = ControladorVentas::ctrSumaTotalVentas();
+
+$categorias = ControladorCategorias::ctrMostrarCategorias($item, $valor);
+$totalCategorias = count($categorias);
+
+$clientes = ControladorClientes::ctrMostrarClientes($item, $valor);
+$totalClientes = count($clientes);
+
+$productos = ControladorProductos::ctrMostrarProductos($item, $valor, $orden);
+$totalProductos = count($productos);
+*/
+?>
+
+
+
+<div class="col-lg-3 col-xs-6">
+
+  <div class="small-box bg-aqua">
+    
+    <div class="inner">
+      
+      <h3>$<?php echo "0.0" ?></h3>
+
+      <p>Cat 1</p>
+    
+    </div>
+    
+    <div class="icon">
+      
+      <i class="ion ion-social-usd"></i>
+    
+    </div>
+    
+    <a href="ventas" class="small-box-footer">
+      
+      Más info <i class="fa fa-arrow-circle-right"></i>
+    
+    </a>
+
+  </div>
+
+</div>
+
+<div class="col-lg-3 col-xs-6">
+
+  <div class="small-box bg-green">
+    
+    <div class="inner">
+    
+    <h3>$<?php echo "0.0" ?></h3>
+
+      <p>Cat 2</p>
+    
+    </div>
+    
+    <div class="icon">
+    
+      <i class="ion ion-clipboard"></i>
+    
+    </div>
+    
+    <a href="categorias" class="small-box-footer">
+      
+      Más info <i class="fa fa-arrow-circle-right"></i>
+    
+    </a>
+
+  </div>
+
+</div>
+
+<div class="col-lg-3 col-xs-6">
+
+  <div class="small-box bg-yellow">
+    
+    <div class="inner">
+    
+    <h3>$<?php echo "0.0" ?></h3>
+
+      <p>Cat 3</p>
+  
+    </div>
+    
+    <div class="icon">
+    
+      <i class="ion ion-person-add"></i>
+    
+    </div>
+    
+    <a href="clientes" class="small-box-footer">
+
+      Más info <i class="fa fa-arrow-circle-right"></i>
+
+    </a>
+
+  </div>
+
+</div>
+
+<div class="col-lg-3 col-xs-6">
+
+  <div class="small-box bg-red">
+  
+    <div class="inner">
+    
+    <h3>$<?php echo "0.0" ?></h3>
+
+      <p>Cat 4</p>
+    
+    </div>
+    
+    <div class="icon">
+      
+      <i class="ion ion-ios-cart"></i>
+    
+    </div>
+    
+    <a href="productos" class="small-box-footer">
+      
+      Más info <i class="fa fa-arrow-circle-right"></i>
+    
+    </a>
+
+  </div>
+
+</div>
