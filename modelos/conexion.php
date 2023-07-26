c1b853ea2a775bd31bd59338ea3ce317f298d9b8
Darwin Flores <daflaco@hotmail.com>
2023-07-26 06:09:47 -0600

    Conexion

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,21 +1,23 @@
 <?php
 
-class Conexion{
+class Conexion
+{
 
-	static public function conectar(){
+	static public function conectar()
+	{
 
-		$link = new PDO("mysql:host=localhost;dbname=grupoiseserver",
-			            "root",
-			            "");
+		$link = new PDO(
+			"mysql:host=localhost;dbname=u900482437_grupoise",
+			"u900482437_user_grupoise",
+			"4LXTbS22w!Iy"
+		);
 
-	/* $link = new PDO("mysql:host=localhost;dbname=armonico_grupoise",
+		/* $link = new PDO("mysql:host=localhost;dbname=armonico_grupoise",
 			            "armonico_grupoise",
 			            "riverPlate11!!"); */
 
 		$link->exec("set names utf8");
 
 		return $link;
-
 	}
-
 }
--- modelos/conexion2.php
+++ modelos/conexion2.php
@@ -1,28 +1,28 @@
 <?php
 
-function Conexion2(){
-	
+function Conexion2()
+{
+
 	/* $dbname='grupoise';  */
-	$dbhost='localhost'; 
-	$dbname='grupoiseserver';
-	$dbusr='root'; 
-	$dbpasw=''; 
+	$dbhost = 'localhost';
+	$dbname = 'u900482437_grupoise';
+	$dbusr = 'u900482437_user_grupoise';
+	$dbpasw = '4LXTbS22w!Iy';
 
 	/* $dbhost='localhost'; 
 	$dbname='armonico_grupoise'; 
 	$dbusr='armonico_grupoise'; 
 	$dbpasw='riverPlate11!!';  */
 
-	
 
-	$db = new mysqli($dbhost,$dbusr,$dbpasw,$dbname);
+
+	$db = new mysqli($dbhost, $dbusr, $dbpasw, $dbname);
 	$db->set_charset("utf8");
 
-	if (!$db){
+	if (!$db) {
 		echo '<div class="error_ mensajes"><strong>Error</strong><br/>Se ha producido un error al conectar a la base de datos</div>';
 		exit();
 	}
-		
+
 	return $db;
-}		
-?>
+}
