bf548fecfc46b76e514102c6f47ffff6fbf7a6ba
Darwin Flores <daflaco@hotmail.com>
2024-03-06 08:52:25 -0600

    Cambios de Reportes corregir vistas

--- vistas/modulos/generarcontratados.php
+++ vistas/modulos/generarcontratados.php
@@ -175,7 +175,7 @@ $ultimoDiaMes = $fecha_actual->format('Y-m-t');
               </div>
             </div>
             <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 text-center">
-              <div id="mensaje"></div>
+              <div id="mensajeview"></div>
             </div>
             <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 text-center">
               <div class="pull-right">
