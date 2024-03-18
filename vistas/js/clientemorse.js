de6cfd73258c49f8b31c11cf20111aa07945e1ef
Darwin Flores <daflaco@gmail.com>
2024-03-17 21:13:22 -0600

    Verificando JS CLienteMorse Mod

--- vistas/js/clientemorse.js
+++ vistas/js/clientemorse.js
@@ -481,41 +481,49 @@ function cerrarModalClienteMorse() {
 
 function llenarSelectMunicipio(departamentoId) {
   // Realizar solicitud AJAX para obtener municipios
-  $.ajax({
-    url: "./ajax/clientemorse.ajax.php",
-    type: "POST",
-    dataType: "json",
-    data: { departamentoId: departamentoId, getMunicipio: "ok" },
-    success: function (data) {
-      // Llenar el select de municipios
-      var municipioSelect = $("#general_id_municipio");
-      municipioSelect.empty(); // Limpiar opciones anteriores
-      // Agregar la opción por defecto
-      municipioSelect.append(
-        '<option value="0" selected>Selecciona un municipio</option>'
-      );
-      $.each(data, function (index, municipio) {
+
+  $("#general_id_municipio").empty();
+  if (departamentoId !== "") {
+    $.ajax({
+      url: "./ajax/clientemorse.ajax.php",
+      type: "POST",
+      dataType: "json",
+      data: { departamentoId: departamentoId, getMunicipio: "ok" },
+      success: function (data) {
+        // Llenar el select de municipios
+        var municipioSelect = $("#general_id_municipio");
+        municipioSelect.empty(); // Limpiar opciones anteriores
+        // Agregar la opción por defecto
         municipioSelect.append(
-          '<option value="' +
-            municipio.id +
-            '">' +
-            municipio.Nombre_m +
-            "</option>"
+          '<option value="0" selected>Selecciona un municipio</option>'
         );
-      });
-    },
-    error: function (xhr, status, error) {
-      var errorMessage = xhr.status + ": " + xhr.statusText;
-      console.log(
-        "Error al obtener municipios. Estado: " +
-          status +
-          ". Error: " +
-          error +
-          ". Detalles: " +
-          errorMessage
-      );
-    },
-  });
+        $.each(data, function (index, municipio) {
+          municipioSelect.append(
+            '<option value="' +
+              municipio.id +
+              '">' +
+              municipio.Nombre_m +
+              "</option>"
+          );
+        });
+      },
+      error: function (xhr, status, error) {
+        var errorMessage = xhr.status + ": " + xhr.statusText;
+        console.log(
+          "Error al obtener municipios. Estado: " +
+            status +
+            ". Error: " +
+            error +
+            ". Detalles: " +
+            errorMessage
+        );
+      },
+    });
+  } else {
+    $("#general_id_municipio").append(
+      '<option value="">Seleciona un municipio</option>'
+    );
+  }
 }
 
 function llenarUltimoEvaluado() {
